5a09069da1d5e4ee40b1f54419d843c487a1ca67
Allen Wight <awight@redhat.com>
2020-08-25 09:36:12 -0400

    Reduce intl logging
    dupe of https://github.com/RedHatInsights/insights-advisor-frontend/pull/737/

--- src/entry.js
+++ src/entry.js
@@ -8,7 +8,7 @@ import { getBaseName } from '@redhat-cloud-services/frontend-components-utilitie
 import messages from '../locales/data.json';
 
 ReactDOM.render(
-    <IntlProvider locale={navigator.language} messages={messages} onError={console.log}>
+    <IntlProvider locale={navigator.language.slice(0, 2)} messages={messages} onError={console.log}>
         <Router basename={getBaseName(window.location.pathname)}>
             <App />
         </Router>
