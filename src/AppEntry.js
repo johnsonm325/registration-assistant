2f59a38a250a97fef6c9aad0ef0e71b66c089a7d
Allen Wight <awight@redhat.com>
2021-01-29 07:28:30 -0500

    Update deps, fec v2 👉 v3 🥰🥰🥰🥰🥰 (#27)
    https://github.com/RedHatInsights/frontend-components/blob/master/packages/components/README.md#migration-v2---v3 IS THE WAY

--- src/AppEntry.js
+++ src/AppEntry.js
@@ -3,7 +3,7 @@ import App from './App';
 import { IntlProvider } from '@redhat-cloud-services/frontend-components-translations';
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
-import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/files/helpers';
+import { getBaseName } from '@redhat-cloud-services/frontend-components-utilities/helpers';
 import messages from '../locales/data.json';
 
 const AppEntry = () => <IntlProvider locale={navigator.language.slice(0, 2)} messages={messages} onError={console.log}>
