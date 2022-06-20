0aef7e524e982f45b3dd0c0699e570daff3d6324
leSamo <soleksak@redhat.com>
2022-06-20 16:31:42 +0200

    Update necessary packages and webpack config

--- config/dev.webpack.config.js
+++ config/dev.webpack.config.js
@@ -1,30 +1,18 @@
 const { resolve } = require('path');
 const config = require('@redhat-cloud-services/frontend-components-config');
 
-const insightsProxy = {
-  https: true,
-  ...(process.env.BETA && { deployment: 'beta/apps' }),
-};
-
-const webpackProxy = {
-  deployment: process.env.BETA ? 'beta/apps' : 'apps',
-  env: process.env.BETA ? 'ci-beta' : 'ci-stable',
-  useProxy: true,
-  useCloud: true,
-  appUrl: process.env.BETA
-    ? ['/beta/insights/registration']
-    : ['/insights/registration'],
-};
-
 const { config: webpackConfig, plugins } = config({
   rootFolder: resolve(__dirname, '../'),
   debug: true,
-  ...(process.env.PROXY ? webpackProxy : insightsProxy),
+  deployment: process.env.BETA ? 'beta/apps' : 'apps',
+  useProxy: true,
+  appUrl: process.env.BETA
+    ? '/beta/insights/registration'
+    : '/insights/registration',
+  env: process.env.BETA ? 'stage-beta' : 'stage-stable',
   exposes: {
     './RootApp': resolve(__dirname, '../src/DevEntry'),
   },
-  env: process.env.BETA ? 'prod-beta' : 'prod',
-  localChrome: process.env.INSIGHTS_CHROME,
 });
 
 plugins.push(
